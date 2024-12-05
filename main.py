b9826345fd6f125e6192db61346657024db3a514
bensouici akram <bensouiciakram@gmail.com>
2024-12-05 06:55:33 +0100

    successfully extracted the text from a test image

--- main.py
+++ main.py
@@ -1,40 +1,50 @@
 import subprocess
+from pathlib import Path 
 from playwright.sync_api import sync_playwright 
 
 
 class GeminiBot:
 
-    def __init__(self,chrome_path:str,port:str):
-        self.chrome_path = chrome_path 
+    def __init__(self,executable:str,port:str):
+        self.executable = executable 
         self.port = port 
 
-    def start_real_browser_instance(self,type:str='chrome'):
-        if type == 'chrome':
-            self.process = subprocess.Popen(
-                [
-                    self.chrome_path,
-                    f'--remote-debugging-port={self.port}'
-                ]
-            )
+    def start_real_browser_instance(self):
+        self.process = subprocess.Popen(
+            [
+                self.executable,
+                f'--remote-debugging-port={self.port}'
+            ]
+        )
 
-    def start_playwright_page(self,type:str='chrome'):
-        self.start_real_browser_instance(type)
+    def start_playwright_page(self):
+        self.start_real_browser_instance()
         playwright = sync_playwright().start()
-        if type == 'chrome':
-            self.browser = playwright.chromium.connect_over_cdp(f"http://localhost:{self.port}")
-        else :
-            self.browser = playwright.firefox.connect_over_cdp(f"http://localhost:{self.port}")
+        self.browser = playwright.chromium.connect_over_cdp(f"http://localhost:{self.port}")
         self.context = self.browser.contexts[0]
         self.page = self.context.new_page()
         self.page.goto('https://gemini.google.com/')
+        self.upload(Path(__file__).parent.joinpath('test.jpeg'))
+        breakpoint()
+        content = self.search('give me the textual content of the uploaded image')
+        breakpoint()
 
     
-
     def search(self,prompt:str) -> str :
         self.page.fill('//rich-textarea//p',prompt)
         self.page.click('//button[contains(@class,"send-button")]')
+        self.page.wait_for_selector('//message-content')
         return self.page.query_selector_all('//message-content')[-1].inner_text()
+    
 
+    def upload(self,file_path:Path):
+        def activate_input_button():
+            with self.page.expect_file_chooser() as fc_info:
+                self.page.click('//button[@id="upload-local-image-button"]')
+
+        activate_input_button()
+        self.page.query_selector('//input[@name="Filedata"]')\
+            .set_input_files(file_path) 
 
 if __name__ == '__main__':
     bot = GeminiBot(
