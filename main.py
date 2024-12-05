686109da454946786c7e9f721ab9b92813ccc2a8
bensouici akram <bensouiciakram@gmail.com>
2024-12-05 08:38:18 +0100

    adding use friendly file for the client

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import sys 
+import json 
 import subprocess
 from pathlib import Path 
+from typing import Optional
 from playwright.sync_api import sync_playwright 
 
 
@@ -8,6 +11,8 @@ class GeminiBot:
     def __init__(self,executable:str,port:str):
         self.executable = executable 
         self.port = port 
+        self.config = self.load_config()
+        self.start_playwright_page() 
 
     def start_real_browser_instance(self):
         self.process = subprocess.Popen(
@@ -20,22 +25,23 @@ class GeminiBot:
     def start_playwright_page(self):
         self.start_real_browser_instance()
         playwright = sync_playwright().start()
-        self.browser = playwright.chromium.connect_over_cdp(f"http://localhost:{self.port}")
+        self.browser = playwright.chromium.connect_over_cdp(
+            f"http://localhost:{self.port}"
+        )
         self.context = self.browser.contexts[0]
         self.page = self.context.new_page()
         self.page.goto('https://gemini.google.com/')
-        self.upload(Path(__file__).parent.joinpath('test.jpeg'))
-        breakpoint()
-        content = self.search('give me the textual content of the uploaded image')
-        breakpoint()
-
     
-    def search(self,prompt:str) -> str :
+    def search_text(self,prompt:str) -> str :
         self.page.fill('//rich-textarea//p',prompt)
         self.page.click('//button[contains(@class,"send-button")]')
-        self.page.wait_for_selector('//message-content')
+        self.page.wait_for_selector('//message-actions')
         return self.page.query_selector_all('//message-content')[-1].inner_text()
-    
+
+    def search(self) -> str:
+        upload_file = self.config['prompt']['file']
+        self.upload(Path(upload_file)) if upload_file else None
+        return self.search_text(self.config['prompt']['text'])
 
     def upload(self,file_path:Path):
         def activate_input_button():
@@ -45,11 +51,20 @@ class GeminiBot:
         activate_input_button()
         self.page.query_selector('//input[@name="Filedata"]')\
             .set_input_files(file_path) 
+        
+    def load_config(self) -> dict:
+        return json.load(open('config.json','r'))
+    
+    def export(self,content:str):
+        with open(Path(self.config['output_file']),'a') as file:
+            file.write(content)
+            file.write('\n\n#---------------------------------#\n\n')
+        self.process.terminate()
+
 
 if __name__ == '__main__':
     bot = GeminiBot(
-        "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
-        port=9222
+        executable=sys.argv[1],
+        port=sys.argv[2]
     )
-    bot.start_playwright_page() 
-    breakpoint()
+    bot.export(bot.search())
---
+++ readme.txt
@@ -0,0 +1,12 @@
+I- Setting up the project : 
+    1- make sure you have python installed in your system and it's added into your envirement variable path (you can check this by typing python in cmd and see if you have a python shell)
+    2- install the requirements by double clicking install_requirements.bat file 
+    3- run the script but double clicking run.bat 
+
+II- Notes: 
+    1- A demonstration demo video is included in the delivery compressed file.
+    2- when running the script close all chrome instances 
+    3- If chrome is not  installed in C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe" correct this path with your correct path by modifying run.bat
+        (just replace C:\\Program Files\\Google\\Chrome\\Application with the folder where you have chrome)
+    4- You can use the script as command line tools by typing -->  python main.py "path_to_your_chrome_executable" port_value (choose and empty port like 9222)
+    5- Unfortunately we can only use chormium based browser (chrome,edge,brave ...etc) to connect over cdp (you can check the note here in documention : https://playwright.dev/python/docs/api/class-browsertype#browser-type-connect-over-cdp)
